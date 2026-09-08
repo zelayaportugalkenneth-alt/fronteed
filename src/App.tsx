3b052ebdb325449a24ed2d3f189aefb40e21e93f
kennethdeep <zelayaportugalkenneth@donboscosucre.edu.bo>
2026-09-08 15:40:44 -0400

    feat: inicializar datos locales al iniciar la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import AppRoutes from "./routes/AppRoutes";
+import { initializeLocalData } from "./utils/localStorageData";
 
+initializeLocalData();
 
 function App() {
   return <AppRoutes />;
 }
-
 
 export default App;
