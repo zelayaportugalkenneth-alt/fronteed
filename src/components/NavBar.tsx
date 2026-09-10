93c15a70d62682ca39c8b4bee74806d817afe515
kennethdeep <zelayaportugalkenneth@donboscosucre.edu.bo>
2026-09-09 21:49:48 -0400

    Fix logo in navigation and remove duplicated school name

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,13 +10,18 @@ function NavBar({ userName, onLogout }: NavBarProps) {
     <header className="nav-bar">
       <nav className="nav-bar__content" aria-label="Navegación principal">
         <a className="nav-bar__brand" href="/" aria-label="Ir al inicio">
-          <img className="nav-bar__images.png" src="/images.png" alt="Colegio Don Bosco Sucre" />
-          <span>Colegio Don Bosco Sucre</span>
+          <img
+            className="nav-bar__logo"
+            src="/Gemini_Generated_Image_h8kc53h8kc53h8kc.jpg"
+            alt="Colegio Don Bosco Sucre"
+          />
         </a>
 
         <div className="nav-bar__actions">
           <span className="nav-bar__user">{userName}</span>
-          <button className="nav-bar__button nav-bar__button--logout" type="button" onClick={onLogout}>Salir</button>
+          <button className="nav-bar__button nav-bar__button--logout" type="button" onClick={onLogout}>
+            Salir
+          </button>
         </div>
       </nav>
     </header>
